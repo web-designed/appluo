70fc9064ec3826434d74220412707c8543887292
kmianowski <krzysztof@creativeconstruction.de>
2018-12-06 14:40:53 +0100

    Comment form validation

--- src/components/CommentAddForm.js
+++ src/components/CommentAddForm.js
@@ -4,27 +4,54 @@ import uuid from 'uuid'
 import { addComment } from '../actions/events'
 import moment from 'moment'
 
-const CommentAddForm = ({ dispatch, currentEventId }) => {
+class CommentAddForm extends React.Component {
 
-   const handleSubmitComment = (e) => {
-      e.preventDefault()
-      const newComment = {
-         commentBody: e.target.commentBody.value,
-         commenter: 'kylu',
-         id: uuid()
-      }
-      dispatch(addComment(currentEventId, newComment))
+   state = {
+      error: '',
+      commentBody: '',
+      commenter: 'kylu'
    }
 
-   return (
-      <div>
-         <h3>Add comment</h3>
-         <form onSubmit={handleSubmitComment}>
-            <textarea name="commentBody"></textarea><br/>
-            <button>Add Comment</button>
-         </form>
-      </div>
-   )
+   onCommentBodyChange = (e) => {
+      const commentBody = e.target.value
+      this.setState(() => ({ commentBody }))
+   }
+
+   handleSubmitComment = (e) => {
+      e.preventDefault()
+      if(!this.state.commentBody){
+         this.setState(() => ({
+            error: 'Please add the comment!'
+         }))
+      } else {
+         const newComment = {
+            commentBody: this.state.commentBody,
+            commenter: this.state.commenter
+         }
+         this.props.dispatch(addComment(this.props.currentEventId, newComment))
+         this.setState(() => ({
+            commentBody: ''
+         }))
+      }
+   }
+
+   render(){
+      return (
+         <div>
+            <h3>Add comment</h3>
+            <form onSubmit={this.handleSubmitComment}>
+               <textarea 
+                  name="commentBody"
+                  value={this.state.commentBody}
+                  onChange={this.onCommentBodyChange}
+                  placeholder="Add your comment"
+               ></textarea><br/>
+               {this.state.error && <span>{this.state.error}</span>}
+               <button>Add Comment</button>
+            </form>
+         </div>
+      )
+   }
 }
 
 export default connect()(CommentAddForm)
