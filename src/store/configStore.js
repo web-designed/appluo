96af6fdcf5f9acce8f23f8a1dff252f50bfa32a5
kmianowski <krzysztof@creativeconstruction.de>
2018-12-07 11:10:06 +0100

    Redux developer tools

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -13,7 +13,8 @@
             settings: settingsReducer,
             events: eventsReducer,
             filters: filtersReducer
-         })
+         }),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
       )
       return store
    }
