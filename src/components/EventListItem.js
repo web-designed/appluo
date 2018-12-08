0c6e02f212f110ff72ad4292c654088153a08556
kmianowski <krzysztof@creativeconstruction.de>
2018-12-08 15:44:31 +0100

    Mark events with notes

--- src/components/EventListItem.js
+++ src/components/EventListItem.js
@@ -4,13 +4,12 @@ import { Link } from 'react-router-dom'
 import moment from 'moment'
 import CommentCount from './CommentCount'
 
-const EventListItem = ({ cleaner, place, cleanedAt, id, comments }) => {
-
+const EventListItem = ({ cleaner, place, cleanedAt, id, comments, note }) => {
    return (
       <div>
          <Link to={`/view/${id}`}>
             <div>
-               <p>{place} | <strong>{cleaner}</strong> | {moment(cleanedAt).format('YYYY-MM-D')} | <CommentCount commentsCount={comments.length}/></p>
+               <p>{place} | <strong>{cleaner}</strong> | {moment(cleanedAt).format('YYYY-MM-D')} | <CommentCount commentsCount={comments.length}/> {note && <span> | N</span>}</p>
             </div>
          </Link>
       </div>
