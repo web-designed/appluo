bcc6de78d5aee085e0d9c3a7f73c7b3bdca5b005
kmianowski <krzysztof@creativeconstruction.de>
2018-12-28 12:46:25 +0100

    minor refactoring

--- src/components/Appluo.js
+++ src/components/Appluo.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import EventsList from './EventsList'
 
-class Appluo extends React.Component {
-   render() {
-      return (
-         <div>
-            <EventsList />
-         </div>
-      )
-   }
-}
+const Appluo = () => (
+   <div>
+      <EventsList />
+   </div>
+)
 
 export default Appluo
