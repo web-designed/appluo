d3e6e541def0624f2e4d0ed96be392a067e7ef2d
kmianowski <krzysztof@creativeconstruction.de>
2019-02-08 15:10:18 +0100

    EDIT EVENT - fix the selected PLACE option

--- src/components/AddEventForm.js
+++ src/components/AddEventForm.js
@@ -5,9 +5,8 @@ import { addEvent } from '../actions/events'
 import moment from 'moment'
 import 'react-dates/initialize'
 import { SingleDatePicker } from 'react-dates'
-import 'react-dates/lib/css/_datepicker.css'
 
-class AddEventForm extends React.Component {
+export class AddEventForm extends React.Component {
 
    state = {
       date: this.props.cleanedAt ? moment(this.props.cleanedAt) : moment(),
@@ -57,18 +56,19 @@ class AddEventForm extends React.Component {
    }
 
    render(){
+
       return(
          <div>
             <form onSubmit={(e) => {this.handleSubmit(e)}}>
                <p>
-                  <label htmlFor="name">Name</label>
+                  <label htmlFor="cleaner">Name</label>
                   <select 
                      name="cleaner" 
                      onChange={this.onCleanerChange}
                      value={this.state.cleaner}
                   >
                      {
-                        this.props.settings.cleaners.map((cleaner) => {
+                        this.props.settings.cleaners.map( cleaner => {
                            return <option value={cleaner.toLowerCase()} key={cleaner}>{cleaner}</option>
                         })
                      }
@@ -79,10 +79,10 @@ class AddEventForm extends React.Component {
                   <select 
                      name="place" 
                      onChange={this.onPlaceChange}
-                     value={this.state.place}
+                     value={this.state.place.toLowerCase()}
                   >
                      {
-                        this.props.settings.places.map((place) => {
+                        this.props.settings.places.map( place => {
                            return <option value={place.toLowerCase()} key={place}>{place}</option>
                         })
                      }
