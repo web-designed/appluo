0f5fe0579e23c6e1753b214fef6ec1303271dc36
kmianowski <krzysztof@creativeconstruction.de>
2019-02-08 16:13:37 +0100

    AddEventForm - testing user interaction (select, textarea)

--- src/tests/components/AddEventForm.test.js
+++ src/tests/components/AddEventForm.test.js
@@ -13,3 +13,30 @@ test('should render the AddEventForm for the Edit Event page correctly', () => {
    const wrapper = shallow(<AddEventForm settings={settings} {...events[0]} />)
    expect(wrapper).toMatchSnapshot()
 })
+
+test('should set the state.cleaner on select option change', () => {
+   const wrapper = shallow(<AddEventForm settings={settings} />)
+   const value = 'kylu'
+   wrapper.find('select').at(0).simulate('change', {
+      target: { value }
+   })
+   expect(wrapper.state('cleaner')).toBe(value)
+})
+
+test('should set the state.place on select option change', () => {
+   const wrapper = shallow(<AddEventForm settings={settings} />)
+   const value = 'bad'
+   wrapper.find('select').at(1).simulate('change', {
+      target: { value }
+   })
+   expect(wrapper.state('place')).toBe(value)
+})
+
+test('should set the state.note on input change', () => {
+   const wrapper = shallow(<AddEventForm settings={settings} />)
+   const value = 'my new note'
+   wrapper.find('textarea').at(0).simulate('change', {
+      target: { value }
+   })
+   expect(wrapper.state('note')).toBe(value)
+})
