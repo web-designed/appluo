48850d66ffbc043f46bcbc3b0f12db8be0dd29bb
kmianowski <krzysztof@creativeconstruction.de>
2019-02-12 12:42:33 +0100

    ADDEVENTPAGE - refactoring

--- src/components/AddEventPage.js
+++ src/components/AddEventPage.js
@@ -3,25 +3,21 @@ import AddEventForm from './AddEventForm'
 import { addEvent } from '../actions/events'
 import { connect } from 'react-redux'
 
-const AddEventPage = (props) => {
-   return(
-      <div>
-         <h1>Add a cleaning</h1>
-         <AddEventForm 
-            handleSubmit={ (event) => {
-               props.onSubmit(event)
-               props.history.push('/')
-            }} 
-            history={props.history} 
-         />
-      </div>
-   )
-}
+const AddEventPage = (props) => (
+   <div>
+      <h1>Add a cleaning</h1>
+      <AddEventForm 
+         handleSubmit={ (event) => {
+            props.onSubmit(event)
+            props.history.push('/')
+         }} 
+         history={props.history} 
+      />
+   </div>
+)
 
-const mapDispatchToProps = (dispatch) => {
-   return {
-      onSubmit: (expense) => dispatch(addEvent(expense))
-   }
-}
+const mapDispatchToProps = (dispatch) => ({
+   onSubmit: (expense) => dispatch(addEvent(expense))
+})
 
 export default connect(undefined, mapDispatchToProps)(AddEventPage)
