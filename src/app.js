f00d75800ee9dac00e349d3d328e01eb78203a44
kmianowski <krzysztof@creativeconstruction.de>
2019-02-18 20:26:22 +0100

    styling HEADER

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { addEvent, addComment } from './actions/events'
 import { setTextFilter, filterByName, filterByPlace } from './actions/filters'
 import moment from 'moment'
 import 'react-dates/lib/css/_datepicker.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
    const store = configStore()
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Header = () => (
-   <header>
-      <h1>Appluo</h1>
-      <p>Für eine schöne ambiente in der WG: {
-         //<strong>{this.state.wgName}</strong>
-      }</p>
-      <div>
-         <NavLink to="/" exact={true}>Dashboard</NavLink> | <NavLink to="/add">Add Event</NavLink> | <NavLink to="/stats">Stats</NavLink>
+export const Header = ({ slogan }) => (
+   <header class="pt-3 bg-dark">
+      <div class="container text-white">
+         <h1 class="h2 mb-0">Appluo</h1>
+         <p>{slogan}</p>
       </div>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-secondary">
+         <div class="container">
+            <ul class="navbar-nav mb-0">
+               <li class="nav-item"><NavLink class="nav-link" to="/" exact={true}>Dashboard</NavLink></li>
+               <li class="nav-item"><NavLink class="nav-link" to="/add">Add Event</NavLink></li>
+               <li class="nav-item"><NavLink class="nav-link" to="/stats">Stats</NavLink></li>
+            </ul>
+         </div>
+      </nav>
    </header>
 )
 
-export default Header
+const mapStateToProps = (state) => ({
+   slogan: state.settings.slogan
+})
+
+export default connect(mapStateToProps)(Header)
