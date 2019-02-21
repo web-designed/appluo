e7aef6d2fe8830f9a88313e001f31bacab0a62e5
kmianowski <krzysztof@creativeconstruction.de>
2019-02-21 17:07:35 +0100

    view event page

--- src/components/ViewEventPage.js
+++ src/components/ViewEventPage.js
@@ -25,26 +25,45 @@ export class ViewEventPage extends React.Component {
    render(){
       if(this.props.event){
          return (
-            <div>
+            <div class="container-fluid">
                {
-                 !this.state.eventDeletedNotification ? (
-                     <div>
-                        <h1>{this.props.event.place}</h1>
-                        <h2>{this.props.event.cleaner} : {moment(this.props.event.cleanedAt).format('YYYY-MM-D')}</h2>
-                        <p>{this.props.event.note}</p>
-                        <p>
-                           <button onClick={this.handleRemoveEvent}>remove</button> 
-                           <Link to={`/edit/${this.props.event.id}`}><button>Edit</button></Link>
-                        </p>
-                        <div>
-                           <CommentsList currentEvent={this.props.event} />
-                           <CommentAddForm currentEventId={this.props.event.id} />
+                  !this.state.eventDeletedNotification ? (
+                     <div class="pt-3 row justify-content-center">
+                        <div class="col-md-6">
+                           <div class="card mb-3">
+                              <div class="card-header d-flex justify-content-between align-items-center">
+                                 <div>
+                                    <h5 class="mb-0">{this.props.event.place}</h5>
+                                 </div>
+                                 <div>
+                                    <small class="text-secondary">cleaned by:</small> {this.props.event.cleaner} <small class="text-secondary">on:</small> {moment(this.props.event.cleanedAt).format('YYYY/MM/D')}
+                                 </div>
+                              </div>
+                              {
+                                 this.props.event.note && (
+                                    <div class="card-body">
+                                       <h6 class="card-subtitle mb-2 text-muted"></h6>
+                                       <p class="card-text">{this.props.event.note}</p>
+                                    </div>
+                                 )
+                              }
+                              <div class="card-footer text-muted">
+                                 <button class="btn btn-danger mr-1" onClick={this.handleRemoveEvent}>remove</button> 
+                                 <Link class="btn btn-primary" to={`/edit/${this.props.event.id}`}>Edit</Link>
+                              </div>
+                           </div>
+                           <div>
+                              <CommentsList currentEvent={this.props.event} />
+                              <CommentAddForm currentEventId={this.props.event.id} />
+                           </div>
                         </div>
                      </div>
                   ) : (
-                     <div>
-                        <h1>{this.state.eventDeletedNotification}</h1>
-                        <Link to="/"><button>Dashboard</button></Link>
+                     <div class="pt-3 row justify-content-center">
+                        <div class="col-md-6">
+                           <h3>{this.state.eventDeletedNotification}</h3>
+                           <Link to="/"><button class="btn btn-primary">Dashboard</button></Link>
+                        </div>
                      </div>
                   )
                }
@@ -52,7 +71,7 @@ export class ViewEventPage extends React.Component {
          )
       } else {
          return (
-            <div>
+            <div class="container-fluid">
                <h1>{this.props.notifications.notFound}</h1>
                <Link to="/"><button>Dashboard</button></Link>
             </div>
