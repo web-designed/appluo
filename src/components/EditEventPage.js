f37da0801d84d0eb38e57e6efe87580c8c93d643
kmianowski <krzysztof@creativeconstruction.de>
2019-02-21 17:07:50 +0100

    edit event page

--- src/components/EditEventPage.js
+++ src/components/EditEventPage.js
@@ -13,25 +13,35 @@ export class EditEventPage extends React.Component {
 
    render(){
       return (
-         <div>
+         <div class="container-fluid">
             {
                this.props.event ? (
-                  <div>
-                     <h1>Edit your cleaning</h1>
-                     <AddEventForm 
-                        history={this.props.history}  
-                        cleaner={this.props.event.cleaner}
-                        place={this.props.event.place}
-                        cleanedAt={this.props.event.cleanedAt}
-                        note={this.props.event.note}
-                        handleSubmit={this.handleSubmit}
-                     />
+                  <div class="pt-3 row justify-content-center">
+                     <div class="col-md-6">
+                        <div class="card">
+                           <div class="card-header">
+                              <h4 class="mb-0">Edit your cleaning</h4>
+                           </div>
+                           <div class="card-body">
+                              <AddEventForm 
+                                 history={this.props.history}  
+                                 cleaner={this.props.event.cleaner}
+                                 place={this.props.event.place}
+                                 cleanedAt={this.props.event.cleanedAt}
+                                 note={this.props.event.note}
+                                 handleSubmit={this.handleSubmit}
+                              />
+                           </div>
+                        </div>
+                     </div>
                   </div>
                ) : (
-                  <div>
-                     <h3>This Cleaning doesn't exist or has been removed</h3>
-                     <Link to="/"><button>Dashboard</button></Link>
-                     <Link to="/add"><button>Add Cleaning</button></Link>
+                  <div class="pt-3 row justify-content-center">
+                     <div class="col-md-6">
+                        <h3>This Cleaning doesn't exist or has been removed</h3>
+                        <Link class="btn btn-secondary mr-2" to="/">Dashboard</Link>
+                        <Link class="btn btn-primary" to="/add">Add Cleaning</Link>
+                     </div>
                   </div>
                )
             }
