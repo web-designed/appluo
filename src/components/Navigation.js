9ccd3570131b5eb3ea450d9f9688e54206ce7ddd
kmianowski <krzysztof@creativeconstruction.de>
2019-02-21 17:08:00 +0100

    Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,9 +5,9 @@ const Navigation = () => {
    return (
       <nav class="navbar navbar-expand-sm bg-dark">
          <ul class="nav nav-pills mb-0">
-            <li class="nav-item"><NavLink activeClassName="active" className="nav-link" to="/" exact={true}>Dashboard</NavLink></li>
-            <li class="nav-item"><NavLink activeClassName="active" className="nav-link" to="/add">Add Event</NavLink></li>
-            <li class="nav-item"><NavLink activeClassName="active" className="nav-link" to="/stats">Stats</NavLink></li>
+            <li class="nav-item"><NavLink activeClassName="active" className="text-white nav-link" to="/" exact={true}>Dashboard</NavLink></li>
+            <li class="nav-item"><NavLink activeClassName="active" className="text-white nav-link" to="/add">Add Event</NavLink></li>
+            <li class="nav-item"><NavLink activeClassName="active" className="text-white nav-link" to="/stats">Chart</NavLink></li>
          </ul>
       </nav>
    )
